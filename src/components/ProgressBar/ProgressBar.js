be0a625b88f3117a935899bd383e6b289f22170f
Jomagene <semjomagene@gmail.com>
2024-04-02 23:22:39 +0200

    Ajout d'une barre de progression globale

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -2,11 +2,10 @@ import React from "react";
 import "./ProgressBar.css";
 
 function ProgressBar({ percentage }) {
-
   return (
     <div className={`progress-bar-container`}>
       <div
-        className={`progress-bar segment-read-color }`}
+        className={`progress-bar segment-read-color`}
         style={{ width: `${percentage}%` }}
       ></div>
     </div>
