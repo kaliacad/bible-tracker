9b977244b98a7acd1217796d61c072a2b21da8aa
firmin nagnduli <finganduli@gmail.com>
2024-04-10 12:27:02 +0200

    improvment: improved the progresss bar in ProgressBar.js

--- src/components/BookDetails.js
+++ src/components/BookDetails.js
@@ -1,5 +1,5 @@
 import { Link, useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { books } from "../data/books";
 import ProgressBar from "./ProgressBar/ProgressBar";
 
